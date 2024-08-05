e9c804dce986c6d1e8270c73330606645fa0f8c8
JXC <jxc@seesafeit.dk>
2024-08-05 16:07:07 +0200

    wqewqe

---
+++ statustracker/update_status.php
@@ -0,0 +1,8 @@
+﻿<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $status = $_POST['status'];
+    file_put_contents('/var/www/html/statustracker/status.txt', $status);
+    echo "Status updated successfully.";
+} else {
+    echo "Invalid request.";
+}
